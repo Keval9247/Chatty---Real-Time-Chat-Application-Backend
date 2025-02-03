0742c49917d709876d74b3fc3fc5aa582ec92d9f
Keval9247 <2102031000011@silveroakuni.ac.in>
2025-02-03 11:24:25 +0530

    Backend for chat app integrated.

---
+++ middleware/socket.js
@@ -0,0 +1,50 @@
+const { Server } = require('socket.io');
+const http = require('http');
+const express = require('express');
+const { userInfo } = require('os');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:5173', 'http://localhost:5174'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+})
+
+
+// used to store online users
+const usersocketMap = {} // {userId: socketId}
+
+const getReceiverSocketId = (userId) => {
+    return usersocketMap[userId];
+}
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+        usersocketMap[userId] = socket.id; // Store socket.id instead of socket
+    }
+
+    io.emit("getOnlineUsers", Object.keys(usersocketMap));
+
+    socket.on("newMessage", (message) => {
+        console.log("New message received by socket:", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+
+        if (userId) {
+            delete usersocketMap[userId];
+            console.log(`Removed userId ${userId} from the map`);
+        }
+
+        io.emit("getOnlineUsers", Object.keys(usersocketMap));
+    });
+});
+
+module.exports = { io, server, app, getReceiverSocketId };
+

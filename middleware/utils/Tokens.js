f571eed9bbb1ae8d74f7419aefc8a8b2e3bc2d21
Keval9247 <2102031000011@silveroakuni.ac.in>
2025-02-03 16:27:47 +0530

    fixing token issue.

--- middleware/utils/Tokens.js
+++ middleware/utils/Tokens.js
@@ -2,18 +2,15 @@ const jwt = require('jsonwebtoken');
 
 exports.generateTokenFun = async (userId, res) => {
     try {
-        // Generate JWT token with user ID
         const token = jwt.sign({ id: userId }, process.env.SECRETKEY, { expiresIn: '1h' });
 
-        // Set the token in a secure HttpOnly cookie
         res.cookie("Bearer", token, {
-            expires: new Date(Date.now() + 60 * 60 * 1000),  // 1 hour expiration
-            httpOnly: true,  // Cookie can't be accessed via JavaScript
-            secure: process.env.NODE_ENV === 'production',  // Secure cookie only in production (HTTPS)
-            sameSite: 'Strict',  // Mitigate CSRF attacks
+            expires: new Date(Date.now() + 60 * 60 * 1000),
+            httpOnly: true,
+            // secure: process.env.NODE_ENV === 'production',
+            sameSite: 'Strict',
         });
 
-        // Return the token, you might want to send this in the response body as well
         return token;
     } catch (error) {
         console.error("Error generating token:", error);
